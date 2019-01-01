5c17c2605cd7a744db793a28acaa1df85246f5c5
Dan Kats <djk175@Dans-MBP.home>
2019-01-01 14:22:16 -0500

    Copyright 2017 --> 2019

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.write('<footer>');
 document.write('<ul class="menu">');
-document.write('	<li class="noclick">&copy 2017 Daniel Kats</li>');
+document.write('	<li class="noclick">&copy 2019 Daniel Kats</li>');
 document.write('	<li style="float:right;" onclick="window.open(\'mailto:dkats12@gmail.com\');">Contact</li>');
 document.write('</ul>');
 document.write('</footer>');
