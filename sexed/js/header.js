cd54fe136ad9aadafa0d31861df834ee3b1e5f9a
Dan Kats <djk175@Dans-MBP.fios-router.home>
2020-02-12 15:16:16 -0500

    Housekeeping

--- sexed/js/header.js
+++ sexed/js/header.js
@@ -15,6 +15,7 @@ function header(level = 0) {
 	document.write('	<li onclick="window.open(\'' + dir + 'anatomy.html\',\'_self\')" id="head_anatomy">Anatomy</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'stis.html\',\'_self\')" id="head_stis">STIs/STDs</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'contraception.html\',\'_self\')" id="head_contra">Contraception</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'sexuality.html\',\'_self\')" id="head_sexuality">Gender & Sexuality</li>');
 	document.write('	<li style="float:right;" onclick="window.open(\'' + dir + 'resources.html\',\'_self\')" id="head_resources">Resources</li>');
 	document.write('</ul>');
 	document.write('</header>');
