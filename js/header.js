c40185322c35cffaaae612125ae5a1becca14e4a
Dan Kats <djk175@Dans-MacBook-Pro.local>
2020-03-07 23:28:59 -0500

    Edits to sexed site

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 document.write('<header>');
 document.write('<ul class="menu">');
 document.write('	<li class="homeclick">KatsMD</li>');
-	document.write('	<li onclick="window.open(\'resources.html\',\'_self\')" id="header_resources">Resources</li>');
+	// document.write('	<li onclick="window.open(\'resources.html\',\'_self\')" id="header_resources">Resources</li>');
 document.write('</ul>');
 document.write('</header>');
