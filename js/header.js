e5faffd4cbe5176959e9730a08e1d8109b06f25a
Dan Kats <djk175@Dans-MBP.fios-router.home>
2020-03-19 23:23:35 -0400

    Added disease pages with COVID-19 being the first

--- js/header.js
+++ js/header.js
@@ -1,6 +1,16 @@
-document.write('<header>');
-document.write('<ul class="menu">');
-document.write('	<li class="homeclick">KatsMD</li>');
-	// document.write('	<li onclick="window.open(\'resources.html\',\'_self\')" id="header_resources">Resources</li>');
-document.write('</ul>');
-document.write('</header>');
+function header(level = 0) {
+	if(level == 0) {
+		dir = '';
+	} else if(level == -1) {
+		dir = '../';
+	}
+
+	document.write('<header>');
+	document.write('<ul class="menu">');
+	document.write('	<li class="homeclick">KatsMD</li>');
+		document.write('	<li onclick="window.open(\'' + dir + 'sexed.html\',\'_self\')" id="header_sexed">Sex Ed</li>');
+		document.write('	<li onclick="window.open(\'' + dir + 'diseases.html\',\'_self\')" id="header_diseases">Diseases</li>');
+		document.write('	<li onclick="window.open(\'' + dir + 'clinicians.html\',\'_self\')" id="header_clinicians">For Clinicians</li>');
+	document.write('</ul>');
+	document.write('</header>');
+}
