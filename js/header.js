45cd75edff4656ce4d20bb2379da7dbfd3c2e637
Dan Kats <dkats12@gmail.com>
2020-09-01 20:57:59 -0400

    Removed header link to sex ed

--- js/header.js
+++ js/header.js
@@ -8,7 +8,7 @@ function header(level = 0) {
 	document.write('<header>');
 	document.write('<ul class="menu">');
 	document.write('	<li class="homeclick">KatsMD</li>');
-		document.write('	<li onclick="window.open(\'' + dir + 'sexed.html\',\'_self\')" id="header_sexed">Sex Ed</li>');
+		// document.write('	<li onclick="window.open(\'' + dir + 'sexed.html\',\'_self\')" id="header_sexed">Sex Ed</li>');
 		document.write('	<li onclick="window.open(\'' + dir + 'diseases.html\',\'_self\')" id="header_diseases">Diseases</li>');
 		document.write('	<li onclick="window.open(\'' + dir + 'clinicians.html\',\'_self\')" id="header_clinicians">For Clinicians</li>');
 	document.write('</ul>');
