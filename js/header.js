ed0d55dbd4df49da3de30b98cc46cfc23f5a0228
Dan Kats <dkats12@gmail.com>
2020-09-09 18:29:01 -0400

    Renamed 'diseases' section to 'info'

--- js/header.js
+++ js/header.js
@@ -7,9 +7,9 @@ function header(level = 0) {
 
 	document.write('<header>');
 	document.write('<ul class="menu">');
-	document.write('	<li class="homeclick">KatsMD</li>');
+	document.write('	<li class="homeclick" onclick="window.open(\'' + dir + 'index.html\',\'_self\')" id="header_home">KatsMD</li>');
 		// document.write('	<li onclick="window.open(\'' + dir + 'sexed.html\',\'_self\')" id="header_sexed">Sex Ed</li>');
-		document.write('	<li onclick="window.open(\'' + dir + 'diseases.html\',\'_self\')" id="header_diseases">Diseases</li>');
+		document.write('	<li onclick="window.open(\'' + dir + 'info.html\',\'_self\')" id="header_info">Patient Information</li>');
 		document.write('	<li onclick="window.open(\'' + dir + 'clinicians.html\',\'_self\')" id="header_clinicians">For Clinicians</li>');
 	document.write('</ul>');
 	document.write('</header>');
