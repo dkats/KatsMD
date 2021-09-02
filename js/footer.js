e2e6f62074b1d819c9e6a6f419f289103ab0dc7b
Dan Kats <dkats12@gmail.com>
2021-09-01 21:57:46 -0400

    Added disclaimer that this is not medical advice

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,7 @@
 document.write('<footer>');
 document.write('<ul class="menu">');
 document.write('	<li class="noclick">&copy 2021 Daniel Kats</li>');
+document.write('	<li class="noclick" style="float:none;">For informational purposes only; not medical advice</li>');
 document.write('	<li style="float:right;" onclick="window.open(\'mailto:dan@katsmd.com\');">Contact</li>');
 document.write('</ul>');
 document.write('</footer>');
