20543c975b7bd3efcaf9fca431254098bbd8c7ef
dkats <dkats@users.noreply.github.com>
2024-01-22 20:36:52 -0500

    2024

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.write('<footer>');
 document.write('<ul class="menu">');
-document.write('	<li class="noclick">&copy 2023 Daniel Kats</li>');
+document.write('	<li class="noclick">&copy 2024 Daniel Kats</li>');
 document.write('	<li class="noclick" style="float:none;">For informational purposes only; not medical advice</li>');
 document.write('	<li style="float:right;" onclick="window.open(\'mailto:dan@katsmd.com\');">Contact</li>');
 document.write('</ul>');
